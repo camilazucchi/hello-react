599c9afb960996a7f0239bfc0379fc7b093bce65
Camila Zucchi <camilazucchi@outlook.com>
2023-04-25 13:53:50 -0300

    style: change html

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,38 @@
 import Banner from "@component/components/banner";
+import Content from "@component/components/content/content";
 import Footer from "@component/components/footer";
 import Navigation from "@component/components/navigation";
+import imageHeader from "/public/assets/img/about.jpg";
+import styles from "../components/content/content.module.css";
 
 export default function About() {
-    return (
-        <main>
-            <Navigation />
-            <Banner />
-            <Footer />
-        </main>
-    )
+  return (
+    <main>
+      <Navigation />
+      <Banner />
+      <Content imageHeader={imageHeader} title="About Me">
+        <p className={styles.paragraph}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus
+          dolor eget purus pretium hendrerit. Proin lobortis interdum leo, ut
+          tincidunt dolor fringilla quis. Sed dignissim, velit vel rhoncus
+          tincidunt, mauris mauris pharetra mi, id volutpat quam arcu ut lorem.
+          Sed in dolor id orci euismod malesuada. Sed gravida quam a arcu
+          vulputate, quis eleifend justo facilisis.<br></br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus
+          dolor eget purus pretium hendrerit. Proin lobortis interdum leo, ut
+          tincidunt dolor fringilla quis. Sed dignissim, velit vel rhoncus
+          tincidunt, mauris mauris pharetra mi, id volutpat quam arcu ut lorem.
+          Sed in dolor id orci euismod malesuada. Sed gravida quam a arcu
+          vulputate, quis eleifend justo facilisis.<br></br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus
+          dolor eget purus pretium hendrerit. Proin lobortis interdum leo, ut
+          tincidunt dolor fringilla quis. Sed dignissim, velit vel rhoncus
+          tincidunt, mauris mauris pharetra mi, id volutpat quam arcu ut lorem.
+          Sed in dolor id orci euismod malesuada. Sed gravida quam a arcu
+          vulputate, quis eleifend justo facilisis.
+        </p>
+      </Content>
+      <Footer />
+    </main>
+  );
 }
