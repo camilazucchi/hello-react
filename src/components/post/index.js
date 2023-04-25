31aba2e750630073e9f2e2207ae6a0c2cdfe6d59
Camila Zucchi <camilazucchi@outlook.com>
2023-04-25 13:52:48 -0300

    feat: add content and post files

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,11 +1,23 @@
-import styles from './post.module.css';
+import Link from "next/link";
+import styles from "./post.module.css";
 
 export default function Post({ post }) {
-    return (
-        <div className={styles.post}>
-            <img className={styles.header} src={`/assets/img/posts/${post.id}/header.jpg`} alt='Capa do post' />
-            <h2 className={styles.title}>{post.title}</h2>
-            <button className={styles.button}>Read</button>
-        </div>
-    )
+  return (
+    <Link
+      href={{
+        pathname: "/post/[id]",
+        query: { id: post.id },
+      }}
+    >
+      <div className={styles.post}>
+        <img
+          className={styles.header}
+          src={`/assets/img/posts/${post.id}/header.jpg`}
+          alt="Capa do post"
+        />
+        <h2 className={styles.title}>{post.title}</h2>
+        <button className={styles.button}>Read</button>
+      </div>
+    </Link>
+  );
 }
