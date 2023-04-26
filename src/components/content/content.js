a91e49588ecc78cf9d8d01570127d19f7f5fabaa
Camila Zucchi <camilazucchi@outlook.com>
2023-04-26 17:19:55 -0300

    add margin and padding styles for the content component

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,11 +1,8 @@
-import Image from "next/image";
-import about from "../../../public/assets/img/about.jpg";
 import styles from "./content.module.css";
 
-export default function Content({ title, children }) {
+export default function Content({ title, children, img }) {
   return (
     <article className={styles.container}>
-        <Image src={about} alt="" className={styles.image}></Image>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.contentContainer}>{children}</div>
     </article>
