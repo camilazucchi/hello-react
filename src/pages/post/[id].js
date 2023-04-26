1b959e7266212e17783dc0c32478c9919a1fecaa
Camila Zucchi <camilazucchi@outlook.com>
2023-04-26 17:22:04 -0300

    feat(post): add commenting functionality to the post page

--- src/pages/post/[id].js
+++ src/pages/post/[id].js
@@ -1,11 +1,36 @@
 import { useRouter } from "next/router";
+import data from "../../json/data.json";
+import Content from "@component/components/content/content";
+import Banner from "@component/components/banner";
+import Navigation from "@component/components/navigation";
+import MarkdownRenderer from "@component/components/markdown";
 
 export default function Posts() {
-    const router = useRouter();
-    const params = router.query.id;
-    console.log(params);
+  const router = useRouter();
+  const params = router.query.id;
+  if (!params) {
+    return <div>Loading...</div>;
+    // adiciona a validação.
+  }
 
-    return (
-        <h1>Teste de post #{params} </h1>
-    )
+  const post = data.find((post) => post.id === Number(params));
+  console.log(post);
+
+  if (!post) {
+    return <h1>Ops, post não encontrado!</h1>;
+    // retorna caso o post não for encontrado.
+  }
+
+  return (
+    <main>
+      <Navigation />
+      <Banner />
+      <Content
+        img={`/public/assets/img/posts/${post.id}/header.jpg`}
+        title={post.title}
+      >
+        <MarkdownRenderer>{post.text}</MarkdownRenderer>
+      </Content>
+    </main>
+  );
 }
