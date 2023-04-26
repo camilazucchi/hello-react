31d31092df91f005febd731a16a15e5774e32e7f
Camila Zucchi <camilazucchi@outlook.com>
2023-04-26 17:19:26 -0300

    feat(banner): add new feature to display a "read more" button

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,13 +1,15 @@
-import styles from "./banner.module.css";
 import Image from "next/image";
-import cato from "../../../public/assets/img/cato.jpg"
+import styles from "./banner.module.css";
+import cato from "../../../public/assets/img/cato.jpg";
 
 export default function Banner() {
+  const { banner, container, title, paragraph, picture } = styles;
+
   return (
-    <div className={styles.banner}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Hello, world!</h1>
-        <p className={styles.paragraph}>
+    <div className={banner}>
+      <div className={container}>
+        <h1 className={title}>Hello, world!</h1>
+        <p className={paragraph}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum
           felis justo, vel bibendum mi rhoncus nec. Fusce quis sapien vitae
           metus rutrum vehicula vel ac ex. Integer vitae tellus in tellus
@@ -15,9 +17,12 @@ export default function Banner() {
         </p>
       </div>
 
-      <Image src={cato}
-      alt="Orange cat licking his nose"
-      className={styles.picture}
+      <Image
+        src={cato}
+        alt="Orange cat licking his nose"
+        className={picture}
+        width={500}
+        height={500}
       />
     </div>
   );
