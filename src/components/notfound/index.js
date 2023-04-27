c604d4e373ca3642a9d31ea0309d90f518637409
Camila Zucchi <camilazucchi@outlook.com>
2023-04-27 13:14:32 -0300

    chore(notfound): import button component

--- src/components/notfound/index.js
+++ src/components/notfound/index.js
@@ -1,25 +1,27 @@
 import Image from "next/image";
 import catCrying from "../../../public/assets/img/catcrying.gif";
 import styles from "./notfound.module.css";
+import Button from "../button";
 
 export default function NotFound() {
   return (
-      <div className={styles.container}>
-        <span className={styles.text404}>404</span>
-        <h1 className={styles.title}>Oops! Page not found.</h1>
-        <p className={styles.text}>
-          The page you are looking for does not exist. <br></br>It might have
-          been moved or deleted.
-        </p>
-        <button className={styles.button}>
-          <a href="/">Return</a>
-        </button>
-        <Image
-          src={catCrying}
-          alt="Cat crying."
-          className={styles.gif}
-          width={200}
-        />
-      </div>
+    <div className={styles.container}>
+      <span className={styles.text404}>404</span>
+      <h1 className={styles.title}>Oops! Page not found.</h1>
+      <p className={styles.text}>
+        The page you are looking for does not exist. <br></br>It might have been
+        moved or deleted.
+      </p>
+      <Button>
+        <a href="/">Return</a>
+      </Button>
+
+      <Image
+        src={catCrying}
+        alt="Cat crying."
+        className={styles.gif}
+        width={200}
+      />
+    </div>
   );
 }
